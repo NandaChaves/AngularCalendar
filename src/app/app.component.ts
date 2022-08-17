9ba5c162c175d1c68da976233b209e0ba9bde396
fernanda fortes <fernandafortesch@gmail.com>
2022-08-18 00:13:07 +0100

    my first angular project

---
+++ src/app/app.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CalendarOptions } from '@fullcalendar/angular';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'angular-calendar-event';
+  Events: any[] = [];
+  calendarOptions: CalendarOptions = {
+    headerToolbar: {
+      left: 'prev,next today',
+      center: 'title',
+      right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
+    },
+    initialView: 'dayGridMonth',
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true
+  };
+  constructor(private httpClient: HttpClient) {}
+  onDateClick(res: any) {
+    alert('Clicked on date : ' + res.dateStr);
+  }
+  ngOnInit() {
+    setTimeout(() => {
+      return this.httpClient
+        .get('http://localhost:8888/event.php')
+        .subscribe((res: any) => {
+          this.Events.push(res);
+          console.log(this.Events);
+        });
+    }, 2200);
+    setTimeout(() => {
+      this.calendarOptions = {
+        initialView: 'dayGridMonth',
+        dateClick: this.onDateClick.bind(this),
+        events: this.Events,
+      };
+    }, 2500);
+  }
+}
